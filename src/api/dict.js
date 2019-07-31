780201570ed70762f508a4ab73155b9995ef7e01
zhangkai <673820543@qq.com>
2019-07-31 18:24:07 +0800

    'dict'

--- src/api/dict.js
+++ src/api/dict.js
@@ -8,8 +8,25 @@ export const getDictAll = () => {
 }
 
 /**
- * 添加字典
+ * 新增或修改字典
  */
-export const addDict = (params) => {
+export const saveOrUpdateDict = (params) => {
   return axios.postRequest('dict/saveOrUpdate', params);
 }
+
+/**
+ * 根据名称模糊查询
+ */
+export const queryByVagueName = (params) => {
+  return axios.getRequest('dict/queryByVagueName', params);
+}
+
+/**
+ * 根据id删除
+ */
+export const deleteById = (params) => {
+  return axios.getRequest('dict/deleteById', params);
+}
+
+
+
--- src/view/dict-manage/index.vue
+++ src/view/dict-manage/index.vue
@@ -2,15 +2,27 @@
   <div>
     <Row>
       <Col span="9" class-name="left">
-        <Input search enter-button placeholder="请输入字典名称" style="width:300px; float:left;"/>
-        <Button @click="showAddDitc" type="primary" icon="md-add" style="margin-left:10px;">添加字典</Button>
-        <Table border :columns="dictlist" :data="dictlistData" style="margin-top: 10px;">
+        <Input
+          search
+          enter-button
+          placeholder="请输入字典名称"
+          v-model="queryVagueName"
+          @on-search="queryByVagueName"
+          style="width:300px; float:left;"
+        />
+        <Button @click="showAddDict" type="primary" icon="md-add" style="margin-left:10px;">添加字典</Button>
+        <Table :columns="dictlist" :data="dictlistData" style="margin-top: 10px;" height="700">
           <template slot-scope="{ row }" slot="name">
             <strong>{{ row.name }}</strong>
           </template>
           <template slot-scope="{ row, index }" slot="action">
-            <Button type="primary" size="small" style="margin-right: 5px" @click="show(index)">修改</Button>
-            <Button type="error" size="small" @click="remove(index)">删除</Button>
+            <Button
+              type="primary"
+              size="small"
+              style="margin-right: 5px"
+              @click="showEditDcit(index)"
+            >修改</Button>
+            <Button type="error" size="small" @click="delDict(index)">删除</Button>
           </template>
         </Table>
       </Col>
@@ -20,7 +32,7 @@
       <Col span="14" class-name="right">col-113132</Col>
     </Row>
 
-    <!-- 添加字典 -->
+    <!-- 添加或更新字典 -->
     <Modal
       :title="dictModalTitle"
       v-model="dictModalVisible"
@@ -44,6 +56,12 @@
           <InputNumber :max="1000" :min="0" v-model="dictForm.sortOrder"></InputNumber>
           <span style="margin-left:5px">值越小越靠前，支持小数</span>
         </FormItem>
+        <FormItem label="是否启用" prop="isAvailable">
+          <i-switch size="large" v-model="dictForm.isAvailable" :true-value="1" :false-value="0">
+            <span slot="open">启用</span>
+            <span slot="close">禁用</span>
+          </i-switch>
+        </FormItem>
       </Form>
       <div slot="footer">
         <Button type="text" @click="dictModalVisible=false">取消</Button>
@@ -54,7 +72,12 @@
 </template>
 
 <script>
-import { getDictAll, addDict } from "@/api/dict";
+import {
+  getDictAll,
+  saveOrUpdateDict,
+  queryByVagueName,
+  deleteById
+} from "@/api/dict";
 import Tables from "_c/tables";
 
 export default {
@@ -68,11 +91,16 @@ export default {
       modalType: 0, // 添加或编辑标识
       dictModalTitle: "",
       dictModalVisible: false,
+      queryVagueName: "", //字典表模糊查询条件
+      dictEditTitle: "", // 编辑标题
+      dictEditVisible: false, // 添加或编辑显示
       dictForm: {
+        id: null,
         name: "",
         code: "",
         description: "",
-        sortOrder: 0
+        sortOrder: 0,
+        isAvailable: 1
       },
       dictFormValidate: {
         // 表单验证规则
@@ -91,11 +119,18 @@ export default {
   watch: {},
   computed: {},
   methods: {
-    showAddDitc() {
+    showAddDict() {
       this.modalType = 0;
-      this.dictModalTitle = "添加字典";
       this.$refs.dictForm.resetFields();
+      this.dictModalTitle = "添加字典";
       this.dictModalVisible = true;
+    },
+    showEditDcit(index) {
+      this.modalType = 1;
+      this.$refs.dictForm.resetFields();
+      this.dictModalTitle = "编辑字典";
+      this.dictModalVisible = true;
+      this.dictForm = this.dictlistData[index];
     },
     handelSubmitDict() {
       this.$refs.dictForm.validate(valid => {
@@ -105,7 +140,7 @@ export default {
           if (this.modalType == 0) {
             // 添加 避免编辑后传入id等数据 记得删除
             delete this.dictForm.id;
-            addDict(this.dictForm).then(res => {
+            saveOrUpdateDict(this.dictForm).then(res => {
               this.submitLoading = false;
               if (res.data.data) {
                 this.$Message.success("操作成功");
@@ -117,9 +152,10 @@ export default {
           //更新
           else if (this.modalType == 1) {
             // 编辑
-            editDict(this.dictForm).then(res => {
+            saveOrUpdateDict(this.dictForm).then(res => {
               this.submitLoading = false;
-              if (res.success) {
+              console.log(res);
+              if (res.data.data) {
                 this.$Message.success("操作成功");
                 this.refreshDictListDate();
                 this.dictModalVisible = false;
@@ -135,6 +171,25 @@ export default {
     refreshDictListDate() {
       getDictAll().then(request => {
         this.dictlistData = request.data.data;
+      });
+    },
+    /**
+     * 根据字典名称查询
+     */
+    queryByVagueName() {
+      queryByVagueName({ name: this.queryVagueName }).then(request => {
+        this.dictlistData = request.data.data;
+      });
+    },
+    /**
+     * 根据id删除字典
+     */
+    delDict(index) {
+      deleteById({ id: this.dictlistData[index].id }).then(res => {
+        if (res.data.data) {
+          this.$Message.success("操作成功");
+          this.refreshDictListDate();
+        }
       });
     }
   },
