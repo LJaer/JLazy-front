1663c6ded31e385d9ba9c48a084483724bdfa145
zhangkai <673820543@qq.com>
2019-08-13 18:48:24 +0800

    'mainvue'

--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -1,22 +1,59 @@
 <template>
-  <div class="wrapper">主页面</div>
+  <Layout>
+    <Header>Header</Header>
+    <Layout>
+      <Sider hide-trigger>
+        <div>
+          <Menu mode="vertical" :theme="theme1" active-name="1">
+            <MenuItem name="1">
+              <Icon type="ios-paper" />内容管理
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-people" />用户管理
+            </MenuItem>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-stats" />统计分析
+              </template>
+              <MenuGroup title="使用">
+                <MenuItem name="3-1">新增和启动</MenuItem>
+                <MenuItem name="3-2">活跃分析</MenuItem>
+                <MenuItem name="3-3">时段分析</MenuItem>
+              </MenuGroup>
+              <MenuGroup title="留存">
+                <MenuItem name="3-4">用户留存</MenuItem>
+                <MenuItem name="3-5">流失用户</MenuItem>
+              </MenuGroup>
+            </Submenu>
+            <MenuItem name="4">
+              <Icon type="ios-construct" />综合设置
+            </MenuItem>
+          </Menu>
+        </div>
+      </Sider>
+      <Content>Content</Content>
+      
+    </Layout>
+    <Footer>Footer</Footer>
+  </Layout>
 </template>
 
 <script>
+
 export default {
-  components:{},
-  props:{},
-  data(){
-    return {
-    }
+  components: {},
+  props: {},
+  data() {
+    return {};
   },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
-}
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
 </script>
 <style lang="scss" scoped>
-.wrapper{}
+.wrapper {
+}
 </style>
